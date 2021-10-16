f70854c4f7fb3c6c248f84937fdc4cf2546b30c6
Cristian Lopez <cristianlopez187@gmail.com>
2021-10-16 12:46:43 -0300

    imagenes de ligas tomadas por api

--- js/apipersona.js
+++ js/apipersona.js
@@ -7,7 +7,9 @@ fetch('http://localhost:3200/personas/listado')
 
             if (x < 6) {
 
-                txt += '<img class=\"img-fluid\" src="' + i.rutaImagen + '"alt=\"...\" />'
+                txt += "<div class=\"col-md-6 col-lg-4 mb-5\"><div class=\"portfolio-item mx-auto\"><div class=\"portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100\"><div class=\"portfolio-item-caption-content text-center text-white\"><i class=\"fas fa-plus fa-3x\"></i></div></div><img class=\"img-fluid\" src=" + i.rutaImagen + " alt=\"...\" /></div></div>"
+
+                //txt += '<img class=\"img-fluid\" src="' + i.rutaImagen + '"alt=\"...\" />'
             }
             x += 1;
 
